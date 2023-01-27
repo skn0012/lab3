735261aee67765d84ff9edd8fd634204292df76e
skn0012 <skn001@ucsd.edu>
2023-01-26 16:57:07 -0800

    1

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,7 +2,11 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
+
+  
+  private static final double DELTA = 1e-15;
+	
+  @Test 
 	public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
@@ -27,5 +31,12 @@ public class ArrayTests {
   public void testBugReversed() {
     int[] input = {0, 1};
     assertArrayEquals(new int[]{1, 0}, ArrayExamples.reversed(input));
+  }
+
+  @Test
+  public void testBugAverageWithoutLowest() {
+    double[] input = {1.0, 1.0, 3.0, 4.0, 5.0};
+    double output = ArrayExamples.averageWithoutLowest(input);
+    assertEquals(3.25, output, DELTA);
   }
 }
